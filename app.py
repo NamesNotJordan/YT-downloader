0a244a005c1bc9e137ad5b767ee03fcfcde542c8
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-03-25 18:12:04 +0200

    moved stuff added stuff. not in the mood for good commits

--- app.py
+++ app.py
@@ -2,21 +2,22 @@ import tkinter as tki
 from pytube import YouTube
 
 DEFAULT_DOWNLOAD = "home/jayden/downloads"
+
+
 def download_video(destination = DEFAULT_DOWNLOAD):
     link = link_entry.get()
-    # audio_only = audio_entry.get()
+    audio_only = audio_only_state.get()
     try:
         yt = YouTube(url=link)
-        
-        yt_stream = yt.streams.get_highest_resolution()
-        
+        if audio_only:
+            yt_stream = yt.streams.get_audio_only()
+        else:
+            yt_stream = yt.streams.get_highest_resolution()
         yt_stream.download(output_path=destination)
 
-    except:
-        print("Download did not work")
-    print("download complete")
-
-
+    except Exception as e:
+        status_label.config(text=f"Error {str(e)}")
+    status_label.config(text="Download Complete")    
 
 
 def testButton():
@@ -36,11 +37,13 @@ status_label = tki.Label(text="")
 status_label.pack
 
 
-link_var = tki.StringVar()
-link_entry = tki.Entry(textvariable=link_var)
+link_entry = tki.Entry()
+link_entry.focus()
 link_entry.pack()
 
 # TODO: add audio only selector
+audio_only_state = tki.BooleanVar()
+audio_only_check = tki.Checkbutton(text="Download audio only",variable= audio_only_state )
 
 download_button = tki.Button(text="Download", command=testButton)
 download_button.pack()
