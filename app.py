ab8e125b0094e5137b49e46ed77003a1d9f33442
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-03-25 17:48:45 +0200

    setup base UI and basic download function

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
 import tkinter as tki
 from pytube import YouTube
 
-def download_video():
-    pass
+DEFAULT_DOWNLOAD = "home/jayden/downloads"
+def download_video(destination = DEFAULT_DOWNLOAD):
+    try:
+        yt = YouTube(url=link_entry.get())
+        yt_stream = yt.streams.get_highest_resolution()
+        
+        yt_stream.download(output_path=destination)
+    except:
+        print("Download did not work")
+    print("download complete")
 
 
-def on_progress(stream, chunk, bytes_remaining):
-    pass
+
+
+def testButton():
+    print(link_entry.get())
+
 
 # Base UI
 app = tki.Tk()
 app.title("Video & Audio Downloader")
+app.minsize(width=500, height=300)
+
+
+heading_label = tki.Label(text="Enter a youtube link")
+heading_label.pack()
+
+status_label = tki.Label(text="")
+status_label.pack
+
+download_button = tki.Button(text="Download", command=testButton)
+download_button.pack()
 
 link_var = tki.StringVar()
 link_entry = tki.Entry(textvariable=link_var)
 link_entry.pack()
+
+app.mainloop()
