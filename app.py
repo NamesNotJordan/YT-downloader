39e4fc1c1037d532ea0083d0978e8d0f152b6a1d
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-03-25 17:52:04 +0200

    minor edit to download_video

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ from pytube import YouTube
 
 DEFAULT_DOWNLOAD = "home/jayden/downloads"
 def download_video(destination = DEFAULT_DOWNLOAD):
+    link = link_entry.get()
+    # audio_only = audio_entry.get()
     try:
-        yt = YouTube(url=link_entry.get())
+        yt = YouTube(url=link)
+        
         yt_stream = yt.streams.get_highest_resolution()
         
         yt_stream.download(output_path=destination)
+
     except:
         print("Download did not work")
     print("download complete")
@@ -31,11 +35,15 @@ heading_label.pack()
 status_label = tki.Label(text="")
 status_label.pack
 
-download_button = tki.Button(text="Download", command=testButton)
-download_button.pack()
 
 link_var = tki.StringVar()
 link_entry = tki.Entry(textvariable=link_var)
 link_entry.pack()
+
+# TODO: add audio only selector
+
+download_button = tki.Button(text="Download", command=testButton)
+download_button.pack()
+
 
 app.mainloop()
